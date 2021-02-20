a9bf585670697a52ab3d46ff3c663ff88f438437
jblose <jamblose@gmail.com>
2021-02-20 02:05:19 -0500

    upgrade to azurerm 2.48.0

--- version.tf
+++ version.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13.0"
   required_providers {
-    azurerm = ">= 2.33.0"
+    azurerm = ">= 2.48.0"
   }
 
   # we provide backend on the CLI and configure via pipeline or env variables. This clears the warnings
